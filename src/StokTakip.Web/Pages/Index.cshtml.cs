5b175d533566645c51ab0a161c53b06c5dc7cc38
cab6y <cihan-abay@hotmail.com>
2023-08-06 23:03:29 +0300

    rev

--- src/StokTakip.Web/Pages/Index.cshtml.cs
+++ src/StokTakip.Web/Pages/Index.cshtml.cs
@@ -1,9 +1,18 @@
-﻿namespace StokTakip.Web.Pages;
+﻿using System;
+using Volo.Abp.Users;
+
+namespace StokTakip.Web.Pages;
 
 public class IndexModel : StokTakipPageModel
 {
+    private readonly ICurrentUser _currentUser;
+    public IndexModel(ICurrentUser currentUser)
+    {
+        _currentUser = currentUser;
+    }
+    public Guid? UserId { get; set; }
     public void OnGet()
     {
-
+        UserId = _currentUser.Id;
     }
 }
